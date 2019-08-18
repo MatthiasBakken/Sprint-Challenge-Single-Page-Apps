2cc3848c24823dafb068bbe0ccdd8ec08a83c0ff
MatthiasBakken <matthias.bakken@gmail.com>
2019-08-18 12:30:55 -0700

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React from 'react';
 import { Route } from 'react-router-dom';
-import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
+import TabNav from './components/TabNav';
+import Header from './components/Header';
+import WelcomePage from './components/WelcomePage';
+import CharacterCard from './components/CharacterCard';
+import CharacterList from './components/CharacterList';
+import LocationsList from './components/LocationsList';
+import LocationCard from './components/LocationCard';
+import EpisodesList from './components/EpisodesList';
+import Episode from './components/Episode';
 
 export default function App() {
   return (
     <main>
-      <Route exact path='/' component={'#'} />
       <Header />
       <TabNav />
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/characters-list' component={CharacterList} />
+      <Route path='/character' component={CharacterCard} />
+      <Route path='/locations-list' component={LocationsList} />
+      <Route path='/location' component={LocationCard} />
+      <Route path='/episodes-list' component={EpisodesList} />
+      <Route path='/episode' component={Episode} />
     </main>
   );
 }

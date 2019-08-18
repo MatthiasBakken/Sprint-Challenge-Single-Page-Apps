c8145fff55b18692a2e7828569b922d4b11a50f6
MatthiasBakken <matthias.bakken@gmail.com>
2019-08-18 12:30:05 -0700

    matthias-bakken initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
   return (
     <main>
+      <Route exact path='/' component={'#'} />
       <Header />
       <TabNav />
-      <AppRouter />
     </main>
   );
 }

89fb96e33f46a4d846934285db3af9b333ca2ce3
MatthiasBakken <matthias.bakken@gmail.com>
2019-08-18 20:29:41 -0700

    made episode card to handle calls to reduce load on api. having too much fun...will receive when I deploy

---
+++ src/components/EpisodeCard.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
+
+import './episode-card.css';
+
+export default function EpisodeCard(props) {
+    const [chars, setChars] = useState([])
+    const [classFlip, setClassFlip] = useState(false)
+
+    const { episode } = props;
+    console.log('one episode\' data', episode)
+    // useEffect(() => {
+    //     setChars(episode.chara)
+    // }, [])
+
+    const fetchCharUrlHandler = () => {
+        setClassFlip(!classFlip);
+        Promise.all(episode.characters.map( async(c) => (
+            await fetch(c).then(res => res.json())
+        ))).then(allResponses => {
+            console.log('all res', allResponses)
+            setChars(allResponses)
+        })
+    }
+
+    const mouseLeaveHandler = () => {
+        setClassFlip(false)
+    }
+
+    return (
+        <div className={classNames({['flip-card']: classFlip})} onMouseLeave={mouseLeaveHandler}>
+            <div className={classNames({['flip-card-inner']: classFlip})}>
+                <div className={classNames({['flip-card-front']: classFlip})}>
+                    <h2>{episode.name}</h2>
+                    <p>{episode.air_date}</p>
+                    <p>Click Here for Character List</p>
+                    <button onClick={fetchCharUrlHandler}>View Characters</button>
+                </div>
+                <div className={classNames({['flip-card-back']: classFlip})}>
+                    {classFlip ? chars.map(c => {
+                        return (
+                            <p key={c.id}>{c.name}</p>
+                        )
+                    }) : null}
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -1,71 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import EpisodeCard from './EpisodeCard';
 
 import './episode-list.css';
 
 const BASE_CLASS = 'episode-card_';
 
 export default function EpisodesList() {
-    const arr = [
-        "https://rickandmortyapi.com/api/character/1",
-        "https://rickandmortyapi.com/api/character/2",
-        "https://rickandmortyapi.com/api/character/35",
-        "https://rickandmortyapi.com/api/character/38",
-        "https://rickandmortyapi.com/api/character/62",
-        "https://rickandmortyapi.com/api/character/127",
-        "https://rickandmortyapi.com/api/character/144",
-        "https://rickandmortyapi.com/api/character/158",
-        "https://rickandmortyapi.com/api/character/175",
-        "https://rickandmortyapi.com/api/character/179",
-        "https://rickandmortyapi.com/api/character/181",
-        "https://rickandmortyapi.com/api/character/239",
-        "https://rickandmortyapi.com/api/character/249",
-        "https://rickandmortyapi.com/api/character/271",
-        "https://rickandmortyapi.com/api/character/338",
-        "https://rickandmortyapi.com/api/character/394",
-        "https://rickandmortyapi.com/api/character/395",
-        "https://rickandmortyapi.com/api/character/435"
-    ]
     // TODO: Add useState to track data from useEffect
     const [episodes, setEpisodes] = useState([]);
     const [page, setPage] = useState(1);
     const [maxPages, setMaxPages] = useState(0);
-    // iterable to hold fetch calls - async operations
-    const [characterFetchCalls, setCharacterFetchCalls] = useState([])
 
     useEffect(() => {
-        
-        Promise.all(
-            arr.map(async (c,i) => {
-                // setCharacterFetchCalls([...characterFetchCalls, await fetch(c)])
-                return await fetch(c).then(res => res.json())
-            }) 
-        ).then(allResponses => {
-            console.log('allResponses', allResponses)
-        })
-        
        // TODO: Add AJAX/API Request here - must run in `useEffect`
         //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
         axios.get(`https://rickandmortyapi.com/api/episode?page=${page}`)
             .then(res => {
                 setEpisodes(res.data.results)
                 setMaxPages(res.data.info.pages)
+                // setCharUrl(res.data.results.characters)
+                // console.log('charactersUrl', res.data.results.characters)
                 console.log('maxPages', res.data.info.pages)
-                console.log(res.data.results)
+                console.log('results', res.data.results)
             })
             .catch(err => {
                 console.log(err.message)
             });
     }, [page]);
-    console.log('characterFetchCalls', characterFetchCalls)
   return (
     <div>
         <section className='episodes-list grid-view'>
         {episodes.map(episode => {
             return (
+                // render EpisodeCard component for every episode
+                // give each EpisodeCard the arr of character URLS
                 <div key={episode.episode} className={`${BASE_CLASS}container`}>
-                <h2 className={`${BASE_CLASS}title`}>{episode.name}</h2>
-
+                    <EpisodeCard key={episode.episode} episode={episode} className={`${BASE_CLASS}`} />
                 </div>
             )
         })}
